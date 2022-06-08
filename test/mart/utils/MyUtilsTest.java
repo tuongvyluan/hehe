603c4107e6743dc5a3cd29245d37d90fbbc8544c
tuongvyluan <luantuongvy13@gmail.com>
2022-06-08 14:02:20 +0700

    hehe

---
+++ test/mart/utils/MyUtilsTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package mart.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luan Tuong Vy
+ */
+public class MyUtilsTest {
+    
+    public MyUtilsTest() {
+    }
+
+    /**
+     * Test of convertDateToLocalDate method, of class MyUtils.
+     */
+    @Test
+    public void testConvertDateToLocalDate() {
+        assertNull(null);
+    }
+    
+}
